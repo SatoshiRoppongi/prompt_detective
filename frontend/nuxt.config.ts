a1ba6aa9a1255185b6e0f4748f11f04cfa3fc9bd
Satoshi Roppongi <satoshiroppongi@gmail.com>
2024-06-08 22:57:39 +0900

    fix usage of vuetify

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,20 +1,33 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    //...
+  ],
   devtools: { enabled: true },
-  buildModules: [
-    '@nuxt/typescript-build',
-    '@nuxtjs/vuetify'
-  ],
-  plugins: [
-    '@/plugins/vuetify',
-    '@/plugins/solana'
-  ],
+  buildModules: ["@nuxt/typescript-build", "@nuxtjs/vuetify"],
+  plugins: ["@/plugins/vuetify", "@/plugins/solana"],
   vite: {
     // plugings: [vuetify()],
     resolve: {
       alias: {
         "@": "/frontend",
+      },
+    },
+
+    vue: {
+      template: {
+        transformAssetUrls
       }
     }
-  }
-})
+  },
+});
--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,22 +1,12 @@
-// plugins/vuetify.ts
-import { createVuetify } from "vuetify";
-import { defineNuxtPlugin } from "#app";
-import "vuetify/styles";
-import { aliases, mdi } from "vuetify/iconsets/mdi";
+// import this after install `@mdi/font` package
 import "@mdi/font/css/materialdesignicons.css";
-import * as components from 'vuetify/components'
 
-export default defineNuxtPlugin((nuxtApp) => {
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+
+export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    components,
-    icons: {
-      defaultSet: "mdi",
-      aliases,
-      sets: {
-        mdi,
-      },
-    },
+    // ... your configuration
   });
-
-  nuxtApp.vueApp.use(vuetify);
+  app.vueApp.use(vuetify);
 });
