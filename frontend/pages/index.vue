cf216302b92634738eb94fc5f46b26ebe8e9242c
Satoshi Roppongi <satoshiroppongi@gmail.com>
2024-06-15 10:03:58 +0900

    プロンプト入力をtextareaにした。また、上に配置した。その他UI微修正

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,155 +1,178 @@
 <template>
-    <v-app>
-        <v-app-bar app>
-            <v-toolbar-title>プロンプト探偵</v-toolbar-title>
-            <v-spacer></v-spacer>
-            <connectWalletButton />
-        </v-app-bar>
-        <v-main>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>プロンプト探偵</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <connectWalletButton />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <h1>画像を生成する元となった文章を当てよう!</h1>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="3">
+            <v-card>
+              <v-card-title>Advertisement</v-card-title>
+              <v-card-text>
+                <!-- Advertisement content -->
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="6">
             <v-container>
-                <v-row>
-                    <v-col cols="12">
-                        <h1>Home Page</h1>
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <v-col cols="3">
-                        <v-card>
-                            <v-card-title>Advertisement</v-card-title>
-                            <v-card-text>
-                                <!-- Advertisement content -->
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                    <v-col cols="6">
-                        <!--
-                        <v-img src="path/to/your/image.jpg" aspect-ratio="1.77"></v-img>
-                    -->
-                        <!--
-                        <v-img
-                            lazy-src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-WZB1pNkPUHzh6UvEIAbkeSpi/user-5riXo2d62EoWc13ZqQ7FDjQE/img-prxolspv0ltdoBIQrsmyATHO.png?st=2024-06-12T07%3A31%3A04Z&se=2024-06-12T09%3A31%3A04Z&sp=r&sv=2023-11-03&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-06-11T19%3A02%3A36Z&ske=2024-06-12T19%3A02%3A36Z&sks=b&skv=2023-11-03&sig=qLE4rmyQH3zjeHkpCaoHmU4aGA%2BqoGslnEEQfasoA9o%3D"
-                            src="https://oaidalleapiprodscus.blob.core.windows.net/private/org-WZB1pNkPUHzh6UvEIAbkeSpi/user-5riXo2d62EoWc13ZqQ7FDjQE/img-prxolspv0ltdoBIQrsmyATHO.png?st=2024-06-12T07%3A31%3A04Z&se=2024-06-12T09%3A31%3A04Z&sp=r&sv=2023-11-03&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2024-06-11T19%3A02%3A36Z&ske=2024-06-12T19%3A02%3A36Z&sks=b&skv=2023-11-03&sig=qLE4rmyQH3zjeHkpCaoHmU4aGA%2BqoGslnEEQfasoA9o%3D"
+              <v-row>
+                <v-col>
+                  <div class="custom-textarea">
+                    <v-form @submit.prevent="submitForm">
+                      <v-textarea
+                        outlined
+                        name="prompt-guessing"
+                        label="プロンプト"
+                        v-model="promptString"
+                        hint="文字数は50文字以下です"
+                        counter
+                        clearable
+                        ref="textareaRef"
+                        @focus="handleFocus"
+                        @blur="handleBlur"
+                      >
+                        <template v-slot:append>
+                          <transition name="fade">
+                            <div
+                              v-if="!focused && !promptString"
+                              class="overlay"
+                              @click="focusTextarea"
                             >
-
-
-                        </v-img>
-                    -->
-                        <v-img
-                            lazy-src="https://picsum.photos/900/300/?random"
-                            src="https://picsum.photos/900/300/?random"
-                            >
-                        </v-img>
-                        
-
-                        <v-form @submit.prevent=" submitForm">
-                            <v-text-field v-model="input" label="Input"></v-text-field>
-                            <v-btn type="submit">Submit</v-btn>
-                            </v-form>
-                    </v-col>
-                    <v-col cols="3">
-                        <v-card>
-                            <v-card-title>Summary Information</v-card-title>
-                            <v-card-text>
-                                <!-- Summary and details about the image -->
-                            </v-card-text>
-                        </v-card>
-                    </v-col>
-                </v-row>
+                              <v-icon large>{{ mdiHelp }}</v-icon>
+                              <div>ここをクリックしてください</div>
+                            </div>
+                          </transition>
+                        </template>
+                      </v-textarea>
+                      <v-btn type="submit">Submit</v-btn>
+                    </v-form>
+                  </div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-img
+                  lazy-src="https://picsum.photos/900/300/?random"
+                  src="https://picsum.photos/900/300/?random"
+                  width="100%"
+                >
+                </v-img>
+              </v-row>
             </v-container>
-        </v-main>
-    </v-app>
+          </v-col>
+          <v-col cols="3">
+            <v-card>
+              <v-card-title>Summary Information</v-card-title>
+              <v-card-text>
+                <!-- Summary and details about the image -->
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import {
-    TransactionInstruction,
-    PublicKey,
-    sendAndConfirmTransaction,
-    Transaction,
-    Connection,
-    Account
-} from '@solana/web3.js';
+import { ref, nextTick } from "vue";
+import { mdiHelp } from "@mdi/js";
 
-import connectWalletButton from '~/components/ConnectWalletButton'
+import connectWalletButton from "~/components/ConnectWalletButton";
 
-const { $solana } = useNuxtApp()
+const input = ref("");
+const promptString = ref("");
+const focused = ref(false);
+const textareaRef = ref(null);
 
-const input = ref('')
+const handleFocus = () => {
+  focused.value = true;
+};
 
-const connectWallet = async () => {
-    try {
-        const { solana } = window as any
-        console.log('aaa')
-        console.log(solana)
-        if (solana && solana.isPhantom) {
-            await solana.connect()
-            console.log('Connected to wallet:', solana.publicKey.toString())
-        } else {
-            alert('Solana wallet not found. Please install Phantom wallet.')
-        }
-    } catch (error) {
-        console.error('Error connecting to wallet:', error)
-    }
-}
+const handleBlur = () => {
+  focused.value = false;
+};
 
-const test = (connection, account) => {
-    const instruction = new TransactionInstruction({
-        keys: [],
-        // sampleプログラムのID
-        programId: new PublicKey('63687Zt1ikkr3ZPTdvPZ6qpbThnWzkzkgqubKVcYFLoE'),
-        data: '',
-    });
-    console.log("account:", account.publicKey.toBase58())
-    sendAndConfirmTransaction(
-        connection,
-        new Transaction().add(instruction),
-        [account],
-        {
-            skipPreflight: true,
-            commitment: "confirmed",
-        },
-    ).then(() => { console.log("done 20240610") }).catch((e) => (console.log('error', e)));
-}
-
+const focusTextarea = () => {
+  focused.value = true;
+  nextTick(() => {
+    textareaRef.value.focus();
+  });
+};
 
 const submitForm = async () => {
-    console.log('Form submitted with input:', input.value);
+  console.log("Form submitted with input:", input.value);
+};
 
-    // プログラムを実行するためのアカウント作成
-    const account = new Account();
-    const lamports = 2 * 1000000000;
-
+const requestAirdropWithRetry = async (
+  publicKey,
+  lamports,
+  retries = 5,
+  delay = 500
+) => {
+  for (let i = 0; i < retries; i++) {
     try {
-        await requestAirdropWithRetry(account.publicKey, lamports);
-        console.log("test airdrop done 20240610");
-        test($solana.connection, account);
+      console.log(`Attempt ${i + 1}: Requesting airdrop...`);
+      // Mocked airdrop request logic
+      return;
     } catch (e) {
-        console.error('Error in submitForm:', e);
+      if (i < retries - 1) {
+        console.log(
+          `Server responded with 429. Retrying after ${delay}ms delay...`
+        );
+        await new Promise((resolve) => setTimeout(resolve, delay));
+        delay *= 2; // Exponential backoff
+      } else {
+        throw e;
+      }
     }
-}
-
-const requestAirdropWithRetry = async (publicKey, lamports, retries = 5, delay = 500) => {
-    for (let i = 0; i < retries; i++) {
-        try {
-            console.log(`Attempt ${i + 1}: Requesting airdrop...`);
-            const signature = await $solana.connection.requestAirdrop(publicKey, lamports);
-            await $solana.connection.confirmTransaction(signature);
-            return;
-        } catch (e) {
-            if (i < retries - 1) {
-                console.log(`Server responded with 429. Retrying after ${delay}ms delay...`);
-                await new Promise(resolve => setTimeout(resolve, delay));
-                delay *= 2; // Exponential backoff
-            } else {
-                throw e;
-            }
-        }
-    }
-}
-
+  }
+};
 </script>
 
 <style scoped>
-/* Add any custom styles here */
+.custom-textarea {
+  position: relative;
+  width: 100%;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5em; /* 文字を大きくする */
+  color: rgba(0, 0, 0, 0.6); /* 色を設定する */
+  cursor: pointer; /* カーソルをポインターにする */
+  transition: opacity 0.5s; /* アニメーションの追加 */
+}
+
+.overlay v-icon {
+  font-size: 2em; /* アイコンを大きくする */
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.custom-textarea textarea {
+  text-align: center;
+}
 </style>
--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -3,10 +3,19 @@ import "@mdi/font/css/materialdesignicons.css";
 
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
+// import { aliases, mdi } from "vuetify/iconsets/mdi";
+import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     // ... your configuration
+    icons: {
+      defaultSet: "mdi",
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
   });
   app.vueApp.use(vuetify);
 });
